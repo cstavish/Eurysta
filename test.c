82b195e947600bfe58a16f6066cc8c6868c74029
Coleman Stavish <ccs@colemanstavish.com>
2011-07-25 00:16:00 -0700

    added compilation instructions for test.c

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "eurysta.h"
 
+// to compile:
+// gcc parser.c c_data_structs/cs_hash_tab.c c_data_structs/cs_linked_list.c object.c -std=c99 test.c -o test -O2
+
 int main(int argc, const char **argv) {
 	cs_json_parser *parser = cs_parser_create_fn("twitter.json");
 	cs_json_obj *root = cs_json_parse(parser);
