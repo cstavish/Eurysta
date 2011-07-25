33371347b62b1a856015cdac08a18c22c6826d64
Coleman Stavish <ccs@colemanstavish.com>
2011-07-25 16:09:36 -0400

    switched to 4 space soft-tabs

--- parser.h
+++ parser.h
@@ -1,70 +1,70 @@
 /*
-	Copyright (c) 2011, Coleman Stavish
-	All rights reserved.
-	
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-		* Redistributions of source code must retain the above copyright
-			notice, this list of conditions and the following disclaimer.
-		* Redistributions in binary form must reproduce the above copyright
-			notice, this list of conditions and the following disclaimer in the
-			documentation and/or other materials provided with the distribution.
-		* Neither the name of Coleman Stavish nor the
-			names of contributors may be used to endorse or promote products
-			derived from this software without specific prior written permission.
-	
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL COLEMAN STAVISH BE LIABLE FOR ANY
-	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    Copyright (c) 2011, Coleman Stavish
+    All rights reserved.
+    
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+        * Redistributions of source code must retain the above copyright
+            notice, this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+            notice, this list of conditions and the following disclaimer in the
+            documentation and/or other materials provided with the distribution.
+        * Neither the name of Coleman Stavish nor the
+            names of contributors may be used to endorse or promote products
+            derived from this software without specific prior written permission.
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL COLEMAN STAVISH BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 enum src_type {
-	SRC_STREAM,
-	SRC_STRING
+    SRC_STREAM,
+    SRC_STRING
 };
 
 enum err_type {
-	ERR_NONE,
-	ERR_ILLEGAL,
-	ERR_NO_MEM,
-	ERR_EXPECTED
+    ERR_NONE,
+    ERR_ILLEGAL,
+    ERR_NO_MEM,
+    ERR_EXPECTED
 };
 
 enum tok_type {
-	TOK_LSQUARE = '[',
-	TOK_RSQUARE = ']',
-	TOK_LCURLY  = '{',
-	TOK_RCURLY  = '}',
-	TOK_COMMA   = ',',
-	TOK_COLON   = ':',
-	TOK_NULL    = 'n',
-	TOK_TRUE    = 't',
-	TOK_FALSE   = 'f',
-	TOK_STRING  = 's',
-	TOK_NUMBER  = '#',
-	TOK_END     = 'e'
+    TOK_LSQUARE = '[',
+    TOK_RSQUARE = ']',
+    TOK_LCURLY  = '{',
+    TOK_RCURLY  = '}',
+    TOK_COMMA   = ',',
+    TOK_COLON   = ':',
+    TOK_NULL    = 'n',
+    TOK_TRUE    = 't',
+    TOK_FALSE   = 'f',
+    TOK_STRING  = 's',
+    TOK_NUMBER  = '#',
+    TOK_END     = 'e'
 };
 
 typedef enum src_type src_t;
-typedef enum err_type	err_t;
+typedef enum err_type   err_t;
 typedef enum tok_type tok_t;
 
 struct cs_json_parser {
-	uint32_t position;
-	union {
-		FILE *stream;
-		const char *string;
-	} source;
-	src_t whence;
-	err_t error;
-	tok_t current;
+    uint32_t position;
+    union {
+        FILE *stream;
+        const char *string;
+    } source;
+    src_t whence;
+    err_t error;
+    tok_t current;
 };
 
 typedef struct cs_json_parser cs_json_parser;
--- test.c
+++ test.c
@@ -5,9 +5,9 @@
 // gcc parser.c c_data_structs/cs_hash_tab.c c_data_structs/cs_linked_list.c object.c -std=c99 test.c -o test -O2
 
 int main(int argc, const char **argv) {
-	cs_json_parser *parser = cs_parser_create_fn("twitter.json");
-	cs_json_obj *root = cs_json_parse(parser);
-	if (root)
-		cs_object_print(root);
-	cs_parser_destroy(parser);
+    cs_json_parser *parser = cs_parser_create_fn("twitter.json");
+    cs_json_obj *root = cs_json_parse(parser);
+    if (root)
+        cs_object_print(root);
+    cs_parser_destroy(parser);
 }
