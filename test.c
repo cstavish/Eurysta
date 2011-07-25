f06ce42d7d91298c9d0f16bcbadc456957ac12ed
Coleman Stavish <ccs@colemanstavish.com>
2011-07-25 16:41:06 -0400

    expanded test program

--- test.c
+++ test.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "eurysta.h"
 
 // to compile:
 // gcc parser.c c_data_structs/cs_hash_tab.c c_data_structs/cs_linked_list.c object.c -std=c99 test.c -o test -O2
 
 int main(int argc, const char **argv) {
-    cs_json_parser *parser = cs_parser_create_fn("twitter.json");
+    cs_json_parser *parser = NULL;
+    if (argc > 1 && strcmp(argv[1], "-") == 0)
+        parser = cs_parser_create_f(stdin);
+    else
+        parser = cs_parser_create_fn("twitter.json");
+
     cs_json_obj *root = cs_json_parse(parser);
+
     if (root)
         cs_object_print(root);
+
     cs_parser_destroy(parser);
 }
